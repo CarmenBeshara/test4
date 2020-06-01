e05eacd977929e78e5e4556c9b00e87ee631c48c
CarmenBeshara <65073274+CarmenBeshara@users.noreply.github.com>
2020-06-01 11:08:24 +0200

    Add files via upload

---
+++ jss.js
@@ -0,0 +1,14 @@
+const countries = ['ilan', 'deenen',  'Ienmark', 'nelly'];
+
+
+  for (let i=0; i<countries.length; i++) {
+    if ( countries[i].startsWith("j") ||countries[i].startsWith("j")) {
+    
+    console.log("Goodbye " + countries[i]);
+    }
+    else
+    {
+      console.log("hello " + countries[i]);
+    }
+  }
+  
